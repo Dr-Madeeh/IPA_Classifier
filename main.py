53d786686ef8478f95db65b6292b4737f2ac8af9
Dr-Madeeh <91950978+Dr-Madeeh@users.noreply.github.com>
2021-10-13 22:39:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -131,7 +131,7 @@ if submit_message:
     fig = px.bar(result,
                 x='predicted_class',
                 y='predicted_prob',
-                hover_name='predicted_class')
+                hover_name='predicted_class', color='predicted_class')
 
     st.plotly_chart(fig)
 ################################################
